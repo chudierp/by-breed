e4afb086fd09442e5200ce531f746bfbee3031a5
Chudier <chudierp@gmail.com>
2022-02-09 19:14:45 -0700

    flatlist of cat breeds

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, FlatList, ScrollView, SafeAreaView, Text, View } from 'react-native';
+
+
+import { cats } from './breeds'
+import Item from './Item'
 
 export default function App() {
+  console.log(cats)
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <FlatList
+        data = {cats}
+        renderItem={({ item, index }) => {
+          return <Item title={`${index} ${item.breed}`} />
+        }}
+        keyExtractor={item => item.breed}
+
+      />
+      
   );
 }
 
