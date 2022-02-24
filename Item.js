29064b730dcd1168c4e8e44f4d3a673b863b4271
Chudier <chudierp@gmail.com>
2022-02-23 19:52:09 -0700

    added features

--- Item.js
+++ Item.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import Feature from './feature'
 
 function Item(props) {
 	const {data} = props
 	const {breed} = data
 	const keys = Object.keys(data).filter(key => key != 'breed')
+
+	const average = (keys.reduce((acc, key) => {
+		return acc + data[key]
+	}, 0) / keys.length).toFixed(1)
 	return (
 		<View style= {styles.container}>
-			<Text style={styles.label}>{breed}</Text>
-			{keys.map(key => <Text>{key} {data[key]}</Text>)}
+			<Text style={styles.label}>{breed} {average}</Text>
+			{/* {keys.map(key => <Text>{key} {data[key]}</Text>)} */}
+			{keys.map(key => <Feature name={key} value={data[key]}/>)}
 		</View>
 	);
 }
