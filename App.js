b33e978e19cef27012c8994be3ed85719a62bebe
Chudier <chudierp@gmail.com>
2022-03-03 16:17:32 -0700

    navigation works

--- App.js
+++ App.js
@@ -1,43 +1,79 @@
+
+   
+import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, FlatList, SafeAreaView, TextInput, View } from 'react-native';
+import {KeyboardAvoidingView, Platform, StyleSheet, View, FlatList, SafeAreaView, TextInput, Text, Button } from 'react-native';
+import Item from './Item';
+import {dogs} from './breeds';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import { cats } from './breeds'
-import Item from './Item'
+import Feature from './feature';
 
-export default function App() {
+function HomeScreen({navigation}) {
   const [search, setSearch] = useState('')
 
   return (
     <SafeAreaView style={styles.container}>
-      <StatusBar style='auto' />
-      <View style={styles.listContainer}>
-        <FlatList
-          data = {cats.filter(item => item.breed.includes(search))}
-          renderItem={({ item }) => {
-            return <Item data={item} />
-          }}
-          keyExtractor={item => item.breed}
+      <StatusBar style="auto" />
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.kav}
+      >
 
+        <View style={styles.listContainer}>
+          <FlatList
+            data={dogs.filter(item  => item.breed.includes(search))}
+            renderItem={({item, index}) => {
+              return (
+              <View style={styles.itemContainer}>
+                <Item dog={item} index={index}/>
+                <Button
+                  title='Details'
+                  onPress={() => navigation.navigate('Features', {
+                  item:{item}
+                  })}
+                />
+              </View>
+              )
+            }}
+            keyExtractor={item => item.breed}
+          />
+          
+        </View>
+        <TextInput 
+          style={styles.search}
+          placeholder='Search Breed'
+          onChangeText={setSearch}
+          value={search}
         />
-      </View>
-      <TextInput 
-        style={styles.search}
-        placeholder='Search'
-        onChangeText={setSearch}
-        value={search}
-      />
+
+      </KeyboardAvoidingView>
     </SafeAreaView>
+
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen}/>
+        <Stack.Screen name="Features" component={Feature}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   listContainer: {
-    width: '100%'
+    width: '100%',
+    flex: 1,
   },
   container: {
-    flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
@@ -47,4 +83,16 @@ const styles = StyleSheet.create({
     padding: 10,
     margin: 2
   },
+  itemContainer: {
+    backgroundColor: 'white',
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 2,
+    paddingTop: 10,
+    paddingBottom: 10,
+    width: 390,
+  }
 });
+
+export default App;
--- Item.js
+++ Item.js
@@ -1,32 +1,28 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
-import Feature from './feature'
+import React from "react";
+import { StyleSheet, View, Text} from 'react-native'
+// import Feature from "./feature";
 
 function Item(props) {
-	const {data} = props
-	const {breed} = data
-	const keys = Object.keys(data).filter(key => key != 'breed')
+    const {dog, index} = props
+    const { breed } = dog
 
-	const average = (keys.reduce((acc, key) => {
-		return acc + data[key]
-	}, 0) / keys.length).toFixed(1)
-	return (
-		<View style= {styles.container}>
-			<Text style={styles.label}>{breed} {average}</Text>
-			{/* {keys.map(key => <Text>{key} {data[key]}</Text>)} */}
-			{keys.map(key => <Feature name={key} key={key} value={data[key]}/>)}
-		</View>
-	);
+    return (
+        <View style={styles.container}>
+            <Text style={styles.header}>{index}: {breed}</Text>
+        </View>
+    )
 }
+
 
 const styles = StyleSheet.create({
 	container: {
-		padding: 10,
-		margin: 1,
-		backgroundColor: 'white'
+		// padding: 10,
+		// margin: 1,
+		backgroundColor: 'white',
+		justifyContent: 'center',
 
 	},
-	label: {
+	header: {
 		fontSize: 20,
 	}
 })
