c550a55a1083773d683e5ddcf5091e70ba8c18eb
Chudier <chudierp@gmail.com>
2022-03-03 17:10:58 -0700

    KeyboardAvoidingViw added

--- Item.js
+++ Item.js
@@ -1,28 +1,34 @@
-import React from "react";
-import { StyleSheet, View, Text} from 'react-native'
-// import Feature from "./feature";
+
+   
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import Feature from './feature'
 
 function Item(props) {
-    const {dog, index} = props
-    const { breed } = dog
+	const {data} = props
+	const {breed} = data
+	const keys = Object.keys(data).filter(key => key != 'breed')
 
-    return (
-        <View style={styles.container}>
-            <Text style={styles.header}>{index}: {breed}</Text>
-        </View>
-    )
+	const average = (keys.reduce((acc, key) => {
+		return acc + data[key]
+	}, 0) / keys.length).toFixed(1)
+	return (
+		<View style= {styles.container}>
+			<Text style={styles.label}>{breed} {average}</Text>
+			{/* {keys.map(key => <Text>{key} {data[key]}</Text>)} */}
+			{keys.map(key => <Feature name={key} key={key} value={data[key]}/>)}
+		</View>
+	);
 }
-
 
 const styles = StyleSheet.create({
 	container: {
-		// padding: 10,
-		// margin: 1,
-		backgroundColor: 'white',
-		justifyContent: 'center',
+		padding: 10,
+		margin: 1,
+		backgroundColor: 'white'
 
 	},
-	header: {
+	label: {
 		fontSize: 20,
 	}
 })
--- feature.js
+++ feature.js
@@ -1,28 +1,17 @@
-import React from "react";
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
 
-import {View, Text, StyleSheet} from 'react-native'
+function Feature({name, value}) {
+    let stars = ''
+    for (let i = 0; i < value; i += 1) {
+        stars += '⭐️'
+    }
 
-function Feature({route}) {
-    const item = route.params.item.item;
-    const keys = Object.keys(item)
     return (
-        <>
-            <Text>{item['breed']}</Text>
-            <View style={styles.keysContainer}>
-                <View>
-                    {keys.filter(key => key != 'breed').map(key => { 
-                    let rate = "";
-                    for(let i = 0; i < item[key]; i += 1) {
-                        rate += "⭐️"
-                    }
-                    return (
-                    // <Feature name={key} value={rate} />
-                        <Text>{key}: {rate}</Text>
-                    )
-                    })}
-                </View>
-            </View>
-        </>
+        <View style={styles.container}>
+            <Text style={styles.name}>{name}</Text>
+            <Text style={styles.value}>{stars}</Text>
+        </View>
     )
 }
 
